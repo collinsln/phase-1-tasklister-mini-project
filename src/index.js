fc3e70023b06129af92aa0f7156192e505d40c7c
collinsln <collangat@gmail.com>
2023-10-03 16:39:36 +0300

    first commit

--- src/index.js
+++ src/index.js
@@ -1,3 +1,23 @@
-document.addEventListener("DOMContentLoaded", () => {
-  // your code here
+
+  document.addEventListener("DOMContentLoaded", () => {
+    // your code here
+    let form = document.querySelector("form")
+    document.querySelector("form").addEventListener('submit',(e) => {
+       e.preventDefault();
+       let inputVal = document.getElementById("new-task-description").value;
+       const ul = document.getElementById("tasks");
+       const btn = document.createElement("button");
+       btn.textContent = "X";
+       btn.addEventListener("click",handleDelete);
+       const li = document.createElement("li");
+       li.textContent = ` ${inputVal} `
+       li.append(btn)
+       ul.append(li)
+       form.reset()
+   
+     })
+   });
+   function handleDelete(e) {
+     console.log(e.target.parentNode.remove())
+   }
 });
